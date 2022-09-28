db73458892c4986de7a52f3f276c7f65cf0ef19c
Pierre Zemb <pierre.zemb@clever-cloud.com>
2022-09-28 19:02:29 +0200

    feat(bindingtester): implement Tenant{Create,SetActive,Delete}

--- foundationdb/src/tenant.rs
+++ foundationdb/src/tenant.rs
@@ -1,8 +1,13 @@
 //! Implementation of the Tenants API
 
-use crate::{error, FdbResult, Transaction};
+use crate::options::TransactionOption;
+
+use crate::{error, Database, FdbBindingError, FdbError, FdbResult, Transaction};
 use foundationdb_sys as fdb_sys;
 use std::ptr::NonNull;
+use std::sync::atomic::{AtomicBool, Ordering};
+
+const TENANT_MAP_PREFIX: &[u8] = b"\xFF\xFF/management/tenant_map/";
 
 pub struct FdbTenant {
     pub(crate) inner: NonNull<fdb_sys::FDBTenant>,
@@ -34,5 +39,96 @@ impl FdbTenant {
         Ok(Transaction::new(NonNull::new(trx).expect(
             "fdb_tenant_create_transaction to not return null if there is no error",
         )))
+    }
+}
+
+/// The FoundationDB API includes function to manage the set of tenants in a cluster.
+// This is a port from https://github.com/apple/foundationdb/blob/87ee0a2963f615079b3f50afa332acd0ead5f1fe/bindings/java/src/main/com/apple/foundationdb/TenantManagement.java
+#[derive(Debug)]
+pub struct TenantManagement;
+
+impl TenantManagement {
+    /// Creates a new tenant in the cluster using a transaction created on the specified Database.
+    /// his operation will first check whether the tenant exists, and if it does it will set the Result
+    /// to a `tenant_already_exists` error. Otherwise, it will attempt to create the tenant in a retry loop.
+    /// If the tenant is created concurrently by another transaction, this function may still return successfully.
+    pub async fn create_tenant(db: &Database, tenant_name: &[u8]) -> Result<(), FdbError> {
+        let checked_existence = AtomicBool::new(false);
+        let checked_existence_ref = &checked_existence;
+
+        let mut key: Vec<u8> = Vec::with_capacity(TENANT_MAP_PREFIX.len() + tenant_name.len());
+        key.extend_from_slice(TENANT_MAP_PREFIX);
+        key.extend_from_slice(tenant_name);
+
+        let key_ref = &key;
+
+        db.run(|trx, _maybe_committed| async move {
+            trx.set_option(TransactionOption::SpecialKeySpaceEnableWrites)?;
+
+            if checked_existence_ref.load(Ordering::SeqCst) {
+                trx.set(key_ref, &[]);
+                Ok(())
+            } else {
+                let maybe_key = trx.get(key_ref, false).await?;
+
+                checked_existence_ref.store(true, Ordering::SeqCst);
+
+                match maybe_key {
+                    None => {
+                        trx.set(key_ref, &[]);
+                        Ok(())
+                    }
+                    Some(_) => {
+                        // `tenant_already_exists` error
+                        Err(FdbBindingError::from(FdbError::new(2132)))
+                    }
+                }
+            }
+        })
+        .await
+        // error can only be an fdb_error
+        .map_err(|e| e.get_fdb_error().unwrap())
+    }
+
+    /// Deletes a tenant from the cluster using a transaction created on the specified `Database`.
+    /// This operation will first check whether the tenant exists, and if it does not it will set the
+    /// result to a `tenant_not_found` error. Otherwise, it will attempt to delete the tenant in a retry loop.
+    /// If the tenant is deleted concurrently by another transaction, this function may still return successfully.
+    pub async fn delete_tenant(db: &Database, tenant_name: &[u8]) -> Result<(), FdbError> {
+        let checked_existence = AtomicBool::new(false);
+        let checked_existence_ref = &checked_existence;
+
+        let mut key: Vec<u8> = Vec::with_capacity(TENANT_MAP_PREFIX.len() + tenant_name.len());
+        key.extend_from_slice(TENANT_MAP_PREFIX);
+        key.extend_from_slice(tenant_name);
+
+        let key_ref = &key;
+
+        db.run(|trx, _maybe_committed| async move {
+            trx.set_option(TransactionOption::SpecialKeySpaceEnableWrites)?;
+
+            if checked_existence_ref.load(Ordering::SeqCst) {
+                trx.clear(key_ref);
+                Ok(())
+            } else {
+                let maybe_key = trx.get(key_ref, false).await?;
+
+                checked_existence_ref.store(true, Ordering::SeqCst);
+
+                match maybe_key {
+                    None => {
+                        // `tenant_not_found` error
+                        Err(FdbBindingError::from(FdbError::new(2131)))
+                    }
+                    Some(_) => {
+                        trx.clear(key_ref);
+                        Ok(())
+                    }
+                }
+            }
+        })
+        .await
+        // error can only be an fdb_error
+        .map_err(|e| e.get_fdb_error().unwrap())
     }
 }
