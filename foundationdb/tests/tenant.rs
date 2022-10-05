feaaa7552d746d2d3c9f521c663041d9d615eb76
Pierre Zemb <pierre.zemb@clever-cloud.com>
2022-10-05 15:57:08 +0200

    feat(foundationdb): fix `list_range` and add tenant::run

--- foundationdb/tests/tenant.rs
+++ foundationdb/tests/tenant.rs
@@ -4,11 +4,14 @@ mod common;
 fn test_tenant() {
     let _guard = unsafe { foundationdb::boot() };
     #[cfg(any(feature = "fdb-7_1",))]
-    futures::executor::block_on(test_tenant_internal()).expect("failed to run");
+    {
+        futures::executor::block_on(test_tenant_management()).expect("failed to run");
+        futures::executor::block_on(test_tenant_run()).expect("failed to run");
+    }
 }
 
 #[cfg(any(feature = "fdb-7_1",))]
-async fn test_tenant_internal() -> foundationdb::FdbResult<()> {
+async fn test_tenant_management() -> foundationdb::FdbResult<()> {
     use foundationdb::tenant::TenantManagement;
 
     let tenant = format!(
@@ -50,5 +53,73 @@ async fn test_tenant_internal() -> foundationdb::FdbResult<()> {
         tenants_size - 1,
         "received a bad list of tenants"
     );
+    Ok(())
+}
+
+#[cfg(any(feature = "fdb-7_1",))]
+async fn test_tenant_run() -> foundationdb::FdbResult<()> {
+    let now = std::time::SystemTime::now()
+        .duration_since(std::time::SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_millis();
+    let db = common::database().await?;
+    // write the same key in multiple tenants
+    for tenant_id in 0..10 {
+        let tenant_name = format!("tenant-{}-{}", now, tenant_id);
+        let tenant_name_ref = &tenant_name;
+
+        foundationdb::tenant::TenantManagement::create_tenant(&db, tenant_name.as_bytes())
+            .await
+            .expect("could not create tenant");
+
+        // open tenant
+        let tenant = db
+            .open_tenant(tenant_name.as_bytes())
+            .expect("could not open tenant");
+
+        // write a key into it
+        tenant
+            .run(|trx, _maybe_committed| async move {
+                trx.set("toto".as_bytes(), tenant_name_ref.as_bytes());
+                Ok(())
+            })
+            .await
+            .expect("could not write key 'toto'");
+    }
+
+    // check that all keys are defined in multiple tenants
+    for tenant_id in 0..10 {
+        let tenant_name = format!("tenant-{}-{}", now, tenant_id);
+
+        let tenant = db
+            .open_tenant(tenant_name.as_bytes())
+            .expect("could not open tenant");
+
+        let slice = tenant
+            .run(|trx, _maybe_committed| async move {
+                Ok(trx
+                    .get("toto".as_bytes(), false)
+                    .await?
+                    .expect("could not find key 'toto'"))
+            })
+            .await
+            .expect("could not read key 'toto'");
+
+        assert_eq!(slice.as_ref(), tenant_name.as_bytes());
+
+        // remove key so that we can delete tenant
+        tenant
+            .run(|trx, _maybe_committed| async move {
+                trx.clear("toto".as_bytes());
+                Ok(())
+            })
+            .await
+            .expect("could not delete key 'toto'");
+
+        foundationdb::tenant::TenantManagement::delete_tenant(&db, tenant_name.as_bytes())
+            .await
+            .expect("could not delete tenant");
+    }
+
     Ok(())
 }
