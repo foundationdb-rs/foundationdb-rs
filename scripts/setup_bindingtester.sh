9fe3364e6d3b5921bf6818b0b5cf3311ad4d2e25
Pierre Zemb <pierre.zemb@clever-cloud.com>
2022-10-06 17:01:22 +0200

    try to add debug to GET_KEY

--- scripts/setup_bindingtester.sh
+++ scripts/setup_bindingtester.sh
@@ -25,6 +25,9 @@ fdboption_file="$(pip show foundationdb | grep Loca | awk '{print $2}')/fdb/fdbo
   sed -i 's/# if op !=/if op != /g' ./bindings/python/tests/tester.py
   sed -i 's/#     print/    print/g' ./bindings/python/tests/tester.py
   sed -i 's/                # print/                print/g' ./bindings/python/tests/tester.py
+  sed -i 's/key, or_equal, offset, prefix = inst.pop(4)/key, or_equal, offset, prefix = inst.pop(4)\n                    print(self.tenant)/' ./bindings/python/tests/tester.py
+
+  cat ./bindings/python/tests/tester.py
 
   echo "testers['rust'] = Tester('rust', '${bindingtester}', 2040, 23, 710, types=ALL_TYPES, tenants_enabled=True)
 " >> ./bindings/bindingtester/known_testers.py
