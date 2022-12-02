2338a97915422fdb91a52c1ea988bd829e2e224c
PierreZ <PierreZ@users.noreply.github.com>
2022-12-02 08:35:19 +0000

    deploy: a77116e806c90a98a01bf5e8823c1672f17a4038

--- implementors/foundationdb/tuple/pack/trait.TuplePack.js
+++ implementors/foundationdb/tuple/pack/trait.TuplePack.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["foundationdb"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"foundationdb":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
