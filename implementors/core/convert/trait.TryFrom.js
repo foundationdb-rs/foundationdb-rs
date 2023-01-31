4db2d69d089a9708e34aa16589756888b2bff832
PierreZ <PierreZ@users.noreply.github.com>
2023-01-31 07:24:03 +0000

    deploy: 0ae70ccec07ca632d58bd5964556d5ca61500623

---
+++ implementors/core/convert/trait.TryFrom.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"foundationdb":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;(&amp;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.67.0/std/primitive.u8.html\">u8</a>], &amp;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.67.0/std/primitive.u8.html\">u8</a>])&gt; for <a class=\"struct\" href=\"foundationdb/tenant/struct.TenantInfo.html\" title=\"struct foundationdb::tenant::TenantInfo\">TenantInfo</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,7 +1,7 @@
 var sourcesIndex = JSON.parse('{\
 "bindingtester":["",[],["main.rs"]],\
 "fdb_bench":["",[],["fdb-bench.rs"]],\
-"foundationdb":["",[["directory",[],["directory_layer.rs","directory_partition.rs","directory_subspace.rs","error.rs","mod.rs","node.rs"]],["tuple",[],["element.rs","hca.rs","mod.rs","pack.rs","subspace.rs","versionstamp.rs"]]],["api.rs","database.rs","error.rs","fdb_keys.rs","future.rs","keyselector.rs","lib.rs","mapped_key_values.rs","options.rs","transaction.rs"]],\
+"foundationdb":["",[["directory",[],["directory_layer.rs","directory_partition.rs","directory_subspace.rs","error.rs","mod.rs","node.rs"]],["tuple",[],["element.rs","hca.rs","mod.rs","pack.rs","subspace.rs","versionstamp.rs"]]],["api.rs","database.rs","error.rs","fdb_keys.rs","future.rs","keyselector.rs","lib.rs","mapped_key_values.rs","options.rs","tenant.rs","transaction.rs"]],\
 "foundationdb_bench":["",[],["lib.rs"]],\
 "foundationdb_gen":["",[],["lib.rs"]],\
 "foundationdb_macros":["",[],["lib.rs"]],\
