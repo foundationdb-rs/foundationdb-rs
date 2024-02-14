7d86ff5568c498bf1df39efcc39623031523ade0
PierreZ <PierreZ@users.noreply.github.com>
2024-02-14 07:13:46 +0000

    deploy: 22da9349df0b21f6b8b3127e922f35c3f7a6bc1f

--- src-files.js
+++ src-files.js
@@ -1,12 +1,12 @@
-var srcIndex = JSON.parse('{\
-"bindingtester":["",[],["main.rs"]],\
-"fdb_bench":["",[],["fdb-bench.rs"]],\
-"foundationdb":["",[["directory",[],["directory_layer.rs","directory_partition.rs","directory_subspace.rs","error.rs","mod.rs","node.rs"]],["tuple",[],["element.rs","hca.rs","mod.rs","pack.rs","subspace.rs","versionstamp.rs"]]],["api.rs","database.rs","error.rs","fdb_keys.rs","future.rs","keyselector.rs","lib.rs","mapped_key_values.rs","options.rs","tenant.rs","transaction.rs"]],\
-"foundationdb_bench":["",[],["lib.rs"]],\
-"foundationdb_gen":["",[],["lib.rs"]],\
-"foundationdb_macros":["",[],["lib.rs"]],\
-"foundationdb_options_gen":["",[],["foundationdb-options-gen.rs"]],\
-"foundationdb_simulation":["",[],["fdb_rt.rs","fdb_wrapper.rs","lib.rs"]],\
-"foundationdb_sys":["",[],["lib.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[\
+["bindingtester",["",[],["main.rs"]]],\
+["fdb_bench",["",[],["fdb-bench.rs"]]],\
+["foundationdb",["",[["directory",[],["directory_layer.rs","directory_partition.rs","directory_subspace.rs","error.rs","mod.rs","node.rs"]],["tuple",[],["element.rs","hca.rs","mod.rs","pack.rs","subspace.rs","versionstamp.rs"]]],["api.rs","database.rs","error.rs","fdb_keys.rs","future.rs","keyselector.rs","lib.rs","mapped_key_values.rs","options.rs","tenant.rs","transaction.rs"]]],\
+["foundationdb_bench",["",[],["lib.rs"]]],\
+["foundationdb_gen",["",[],["lib.rs"]]],\
+["foundationdb_macros",["",[],["lib.rs"]]],\
+["foundationdb_options_gen",["",[],["foundationdb-options-gen.rs"]]],\
+["foundationdb_simulation",["",[],["fdb_rt.rs","fdb_wrapper.rs","lib.rs"]]],\
+["foundationdb_sys",["",[],["lib.rs"]]]\
+]'));
 createSrcSidebar();
