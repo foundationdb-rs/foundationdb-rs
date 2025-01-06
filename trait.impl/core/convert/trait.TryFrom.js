21765686183ac78ffd1292e964cb613727141160
PierreZ <PierreZ@users.noreply.github.com>
2025-01-06 10:04:06 +0000

    deploy: 88b92ae49d9228ffe11b956adbab139384972375

--- trait.impl/core/convert/trait.TryFrom.js
+++ trait.impl/core/convert/trait.TryFrom.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["foundationdb",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;(&amp;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.82.0/std/primitive.u8.html\">u8</a>], &amp;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.82.0/std/primitive.u8.html\">u8</a>])&gt; for <a class=\"struct\" href=\"foundationdb/tenant/struct.TenantInfo.html\" title=\"struct foundationdb::tenant::TenantInfo\">TenantInfo</a>"]]]]);
+    var implementors = Object.fromEntries([["foundationdb",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;(&amp;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.83.0/std/primitive.u8.html\">u8</a>], &amp;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.83.0/std/primitive.u8.html\">u8</a>])&gt; for <a class=\"struct\" href=\"foundationdb/tenant/struct.TenantInfo.html\" title=\"struct foundationdb::tenant::TenantInfo\">TenantInfo</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
