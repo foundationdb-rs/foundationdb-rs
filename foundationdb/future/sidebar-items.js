176f74ef5acdb0cddcf5d625576f0ef9b44b9126
PierreZ <PierreZ@users.noreply.github.com>
2026-01-12 16:37:53 +0000

    deploy: 5ec586c8dbf824c9f87594f685b7dfdd7280566e

--- foundationdb/future/sidebar-items.js
+++ foundationdb/future/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["FdbAddress","FdbAddresses","FdbKeyValue","FdbSlice","FdbValue","FdbValues","FdbValuesIter"]};
+window.SIDEBAR_ITEMS = {"static":["CUSTOM_EXECUTOR_HOOK"],"struct":["FdbAddress","FdbAddresses","FdbKeyValue","FdbSlice","FdbValue","FdbValues","FdbValuesIter"]};
