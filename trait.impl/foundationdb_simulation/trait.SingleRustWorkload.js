a897654e42aa70a50816761110b5dee78e62f173
PierreZ <PierreZ@users.noreply.github.com>
2026-09-16 11:30:05 +0000

    deploy: b52ec8892c713e169334b80004c106e911682563

--- trait.impl/foundationdb_simulation/trait.SingleRustWorkload.js
+++ trait.impl/foundationdb_simulation/trait.SingleRustWorkload.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["foundationdb_recipes_simulation",[["impl SingleRustWorkload for <a class=\"struct\" href=\"foundationdb_recipes_simulation/struct.LeaderElectionWorkload.html\" title=\"struct foundationdb_recipes_simulation::LeaderElectionWorkload\">LeaderElectionWorkload</a>"]]]]);
+    var implementors = Object.fromEntries([["foundationdb_recipes_simulation",[["impl <a class=\"trait\" href=\"foundationdb_simulation/trait.SingleRustWorkload.html\" title=\"trait foundationdb_simulation::SingleRustWorkload\">SingleRustWorkload</a> for <a class=\"struct\" href=\"foundationdb_recipes_simulation/struct.LeaderElectionWorkload.html\" title=\"struct foundationdb_recipes_simulation::LeaderElectionWorkload\">LeaderElectionWorkload</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[265]}
+//{"start":57,"fragment_lengths":[411]}
